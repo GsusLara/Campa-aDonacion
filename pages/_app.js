899948580298169fbf4291bcbc7b3cb97c574426
GsusLara <jelara99@gmail.com>
2021-11-08 15:09:09 -0600

    correciones 10

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import '../styles/globals.scss'
 import injectContext from "../store/appContext";
 import { fas } from "@fortawesome/free-solid-svg-icons"
@@ -9,7 +10,14 @@ library.add(fas, fab, far)
 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossOrigin="anonymous"></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default injectContext(MyApp)
