5aea46cb2fee03322f519c051fc8c1d54ae823c7
GsusLara <jelara99@gmail.com>
2021-11-08 15:19:27 -0600

    correciones 11

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import { useEffect } from "react";
 import '../styles/globals.scss'
 import injectContext from "../store/appContext";
 import { fas } from "@fortawesome/free-solid-svg-icons"
@@ -10,14 +10,10 @@ library.add(fas, fab, far)
 
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossOrigin="anonymous"></script>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+  return <Component {...pageProps} />
 }
 
 export default injectContext(MyApp)
